85a55a8f848e12d3a191d3e2334f7464f66f881c
Daniel <danielviei@hotmail.com>
2021-07-19 20:24:40 -0400

    feat: add part of userResource to test it

--- app/users/api_v1_0/schemas.py
+++ app/users/api_v1_0/schemas.py
@@ -2,12 +2,12 @@ from marshmallow import fields
 
 from app.ext import marsh
 
-class userSchema(marsh.Schema):
+class UserSchema(marsh.Schema):
     id = fields.Integer()
     username = fields.String()
     mail = fields.String()
 
-class postSchema(marsh.Schema):
+class PostSchema(marsh.Schema):
     id = fields.Integer()
     user = fields.Integer()
     date = fields.DateTime
